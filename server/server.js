5f9b3898fbeb95201b877af3d8f0bff2afd68815
Yana <yana@MacBook-Air13-right.local>
2020-07-01 02:35:46 +0300

    repo is made

--- server/server.js
+++ server/server.js
@@ -4,9 +4,32 @@ import path from 'path'
 import cors from 'cors'
 import bodyParser from 'body-parser'
 import sockjs from 'sockjs'
+import axios from 'axios'
 
 import cookieParser from 'cookie-parser'
 import Html from '../client/html'
+
+const { readFile, writeFile, unlink } = require('fs').promises
+
+const setHeaders = (req, res, next) => {
+  res.set('x-skillcrucial-user', '9316f92b-9d2a-4223-822d-03d00fe89587')
+  res.set('Access-Control-Expose-Headers', 'X-SKILLCRUCIAL-USER')
+  return next()
+}
+
+const saveFile = async (users) => {
+  return writeFile(`${__dirname}/test.json`, JSON.stringify(users), { encoding: 'utf8' })
+}
+
+const fileRead = async () => {
+  return readFile(`${__dirname}/test.json`, { encoding: 'utf8' })
+    .then((data) => JSON.parse(data))
+    .catch(async () => {
+      const { data: users } = await axios('https://jsonplaceholder.typicode.com/users')
+      await saveFile(users)
+      return users
+    })
+}
 
 let connections = []
 
@@ -20,6 +43,47 @@ server.use(bodyParser.urlencoded({ limit: '50mb', extended: true, parameterLimit
 server.use(bodyParser.json({ limit: '50mb', extended: true }))
 
 server.use(cookieParser())
+
+server.use(setHeaders)
+
+server.get('/api/v1/users', async (req, res) => {
+  const users = await fileRead()
+  res.json(users)
+})
+
+server.post('/api/v1/users', async (req, res) => {
+  const users = await FileReader()
+  const newUserBody = req.body
+  const userLength = users[users.length - 1].id
+  newUserBody.id = userLength + 1
+  const newUser = [...users, newUserBody]
+  saveFile(newUser)
+  res.json({ status: 'success', id: newUserBody.id })
+})
+
+server.patch('/api/v1/users/:userId', async (req, res) => {
+  const users = await fileRead()
+  const { userId } = req.parse
+  const newUserBody = req.body
+  const newUsersArray = users.map((it) =>
+    it.id === Number(userId) ? Object.assign(it, newUserBody) : it
+  )
+  saveFile(newUsersArray)
+  res.json({ status: 'success', id: userId })
+})
+
+server.delete('/api/v1/users/:userId', async (req, res) => {
+  const users = await fileRead()
+  const { userId } = req.params
+  users.splice(Number(userId) - 1, 1)
+  saveFile(users)
+  res.json({ status: 'success', id: userId })
+})
+
+server.delete('/api/v1/users/:userId', async (req, res) => {
+  unlink(`${__dirname}/test.json`)
+  res.json()
+})
 
 server.use('/api/', (req, res) => {
   res.status(404)
